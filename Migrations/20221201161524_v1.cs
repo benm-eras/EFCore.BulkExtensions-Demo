2a3c8a290f49f943fdc42976e1c41f84f861ddcd
Ben Miller <BenM@eras.co.uk>
2022-12-01 16:29:00 +0000

    Add project files.

---
+++ Migrations/20221201161524_v1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EFCoreDemo;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureHostConfiguration(builder =>
+            {
+                builder.AddUserSecrets(typeof(Program).Assembly);
+            })
+            .ConfigureServices((b, services) =>
+            {
+                string cs = b.Configuration.GetConnectionString("SqlLocal") ?? throw new ArgumentNullException();
+                services.AddDbContextFactory<DataContext>((services, builder) =>
+                {
+                    builder.UseSqlServer(cs);
+                });
+            })
+            .Build();
+
+        using DataContext context = host.Services.GetRequiredService<IDbContextFactory<DataContext>>().CreateDbContext();
+
+        IQueryable<Blog> query = context.Blogs.Where(b => b.Id > 0);
+
+        // throws System.MissingMethodException here
+        int deleted = await query.BatchDeleteAsync();
+        
+        // standard EF version is fine
+        // foreach (Blog blog in query)
+        //    context.Remove(blog);
+        //
+        // int deleted = await context.SaveChangesAsync();
+
+        Console.WriteLine($"Deleted {deleted} blogs");
+
+        await host.StopAsync();
+    }
+}
